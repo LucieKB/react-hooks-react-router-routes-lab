a7db6a0788c5c2a16d753e7e935122e3d01795c5
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-03-21 15:45:14 -0600

    commit tree 2a5501598855c102b8d172b056660295424cf320

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { directors } from "../data";
 
+const allDirectors = 
+directors.map((director)=>(
+<div key={director.name}>
+  {director.name}
+  <ul>
+    {director.movies.map((movie)=> 
+    <li key={movie}>{movie}</li>)}
+  </ul>
+</div>
+)
+)
+
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (
+  <div>
+    <h1>Directors Page</h1>
+{allDirectors} 
+  </div>
+)}
 
 export default Directors;
+
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { movies } from "../data";
 
+const moviesToReturn = 
+movies.map((movie)=>(
+<div key={movie.title}>
+  {movie.title}
+  {movie.time}
+  <ul>
+    {movie.genres.map((genre)=>(
+        <li key={genre}> 
+          {genre}
+        </li>
+      )
+    )}
+  </ul>
+</div>
+  )
+);
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {moviesToReturn}
+  </div>
+  )}
 
 export default Movies;
